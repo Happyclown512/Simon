fdf1c458d3dc7d8f659421ce006de12892382136
CS <happyclown512@gmail.com>
2022-09-27 12:43:31 -0500

    working?

--- Simon/ViewController.swift
+++ Simon/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var RedButton: UIButton!
     @IBOutlet weak var YellowButton: UIButton!
     @IBOutlet weak var BlueButton: UIButton!
@@ -17,22 +17,21 @@ class ViewController: UIViewController {
     var audioPlayer = AVAudioPlayer()
     var timer:Timer = Timer()
     var r = 2
-    
+    var seq = [Int]()
+    var input = [Int]()
+    var wrong = [Bool]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         genSeq(rounds: r)
-        
     }
     
-    func genSeq(rounds: Int) -> Void{
+    func genSeq(rounds: Int) -> Void {
         var round = 1
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { Timer in
             let c = Int.random(in: 1...4)
             self.sound(c: c)
-            
-            
+            self.seq.append(c)
             
             if(round >= rounds){
                 Timer.invalidate()
@@ -40,13 +39,10 @@ class ViewController: UIViewController {
                 round += 1
             }
         })
-        
-        
-        
-        
     }
     
-     func sound(c: Int) -> Void {
+    
+    func sound(c: Int) -> Void {
         if( c == 1 ){
             print("red")
             self.playRed()
@@ -60,14 +56,16 @@ class ViewController: UIViewController {
             self.playBlue()
         }
         else if ( c == 4) {
-        print("yellow")
-        self.playYellow()
+            print("yellow")
+            self.playYellow()
         }
     }
     
-
     @IBAction func onRed(_ sender: Any) {
         playRed()
+        input.append(1)
+        
+        checkInput()
     }
     func playRed(){
         let sound = Bundle.main.url(forResource: "red", withExtension: "wav")
@@ -77,12 +75,14 @@ class ViewController: UIViewController {
         while !audioPlayer.isPlaying{
             audioPlayer.play()
         }
-
     }
     
     
     @IBAction func onGreen(_ sender: Any) {
         playGreen()
+        input.append(2)
+        
+        checkInput()
     }
     func playGreen(){
         let sound = Bundle.main.url(forResource: "green", withExtension: "wav")
@@ -90,12 +90,15 @@ class ViewController: UIViewController {
         while !audioPlayer.isPlaying{
             audioPlayer.play()
         }
-
+        
     }
     
     
     @IBAction func onBlue(_ sender: Any) {
         playBlue()
+        input.append(3)
+        
+        checkInput()
     }
     func playBlue(){
         let sound = Bundle.main.url(forResource: "blue", withExtension: "wav")
@@ -103,13 +106,16 @@ class ViewController: UIViewController {
         while !audioPlayer.isPlaying{
             audioPlayer.play()
         }
-
+        
         
     }
     
     
     @IBAction func onYellow(_ sender: Any) {
         playYellow()
+        input.append(4)
+        
+        checkInput()
     }
     func playYellow(){
         let sound = Bundle.main.url(forResource: "yellow", withExtension: "wav")
@@ -120,6 +126,33 @@ class ViewController: UIViewController {
         }
     }
     
+    func checkInput(){
+        let c = seq.count
+        if( seq.count == input.count ){
+            
+            for i in 0...c-1{
+                print(seq[i] == input[i])
+                if ( seq[i] != input[i] ){
+                    let sound = Bundle.main.url(forResource: "lose", withExtension: "wav")
+                    audioPlayer = try! AVAudioPlayer(contentsOf: sound!)
+                    audioPlayer.play()
+                    
+                    wrong.append(false)
+                    
+                } else {
+                    wrong.append(true)
+                }
+            }
+            if( !(wrong.contains(false))){
+                let sound = Bundle.main.url(forResource: "hiscore", withExtension: "wav")
+                audioPlayer = try! AVAudioPlayer(contentsOf: sound!)
+                audioPlayer.play()
+
+                r = r + 1
+                genSeq(rounds: r)
+            }
+        }
+        
+    }
     
 }
-
